d3771b0c7fe6a4208a401199213197d6817637a3
Jeonghoon2 <ukidd12@naver.com>
2024-03-30 08:22:31 +0900

    feat: json_to_parquet의 Airflow Dag 개발

--- Effort-Etl/dag/json_to_parquet_dag.py
+++ Effort-Etl/dag/json_to_parquet_dag.py
@@ -1,26 +1,51 @@
-from datetime import datetime, timedelta
-
+import os
+from datetime import timedelta, datetime
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from operators.spark import PysparkClusterOperator, SparkJobResource
+
+default_args = {
+    'owner': 'altera',
+    'retries': 0,
+    'retry_delay': timedelta(minutes=2),
+    'execution_timeout': timedelta(minutes=120)
+}
+
+DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
 
 with DAG(
-        'json_to_parquet_dag',
-        default_args={
-            'owner': 'airflow',
-            'start_date': datetime(2024, 1, 14)
-        },
-        schedule_interval=timedelta(days=1),
+        dag_id=DAG_ID,
+        default_args=default_args,
+        schedule_interval='0 9 * * *',
         catchup=False,
+        start_date=datetime(2024, 1, 20),
+        dagrun_timeout=timedelta(hours=8),
+        max_active_runs=3,
+        concurrency=1,
+        tags=['ingestion', 'json', 'parquet']
 ) as dag:
-    bash_task = BashOperator(
-        task_id='jtp_task',
-        bash_command='/usr/bdp/airflow/dags/airflow-de/jtp_app_submit.sh client {{ ds }} prod',
-        env={
-            'HADOOP_CONF_DIR': '/usr/bdp/hadoop/etc/hadoop',
-            'SPARK_HOME': '/usr/bdp/spark',
-            'PYSPARK_PYTHON': '/usr/bin/python'
-        },
-        dag=dag,
+    airflow_root = "/usr/bdp/airflow/dags"
+    execution_date = "{{ execution_date.in_timezone('Asia/Seoul').strftime('%Y-%m-%d') }}"
+    json_to_parquet_task = PysparkClusterOperator(
+        task_id="json_to_parquet",
+        source_path=f"{airflow_root}/spark/json_to_parquet.py",
+        job_resource=SparkJobResource(
+            driver_core=1,
+            driver_mem="2G",
+            executor_num=2,
+            executor_core=1,
+            executor_mem="2G"
+        ),
+        additional_arguments=[
+            "--base_dt", execution_date,
+            "--run_env", "prod"
+        ],
+        py_files=[
+            f"{airflow_root}/spark/utils/spark.py",
+            f"{airflow_root}/spark/utils/etl_base.py"
+        ],
+        hadoop_user_name="hive",
+        python_env_name="bdp-default",
+        python_env_version="latest",
     )
 
-    bash_task
+    json_to_parquet_task
